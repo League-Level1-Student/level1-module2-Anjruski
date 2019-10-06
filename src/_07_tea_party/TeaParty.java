e776e7d8f04e2bd6a85e01aec7ba65632ffe0d42
League Student <league@iMac-21.attlocal.net>
2019-10-06 14:27:50 -0700

    whoops
    :cry: :cry: :cry:

--- src/_07_tea_party/TeaParty.java
+++ src/_07_tea_party/TeaParty.java
@@ -1,5 +1,29 @@
 package _07_tea_party;
 
-public class TeaParty {
+import javax.swing.JOptionPane;
 
+public class TeaParty {
+      
+		public String welcome(String name, boolean isWoman, boolean isKnighted) {
+			String Welcome = "";
+			if(isWoman) {
+				if(isKnighted) {
+				Welcome = "Hello Lady " + name;
+				}
+				else {
+					Welcome = "Hello Ms." + name;
+				}
+			}
+			else {
+				Welcome = "Hello Mr." + name;
+			}
+			
+			
+			
+			
+			
+			
+			return Welcome;
+        }
 }
+
